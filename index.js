ab39505b5f58e6a56b3601a162184c9bf7fcd16c
Renato Tenório Lino <103604513+renatotl@users.noreply.github.com>
2022-09-28 11:19:28 -0300

    fix: cors antes da rota

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const port = 3000;
 const app = express();
 const route = require('./src/routes/characters.route');
 
+app.use(cors());
 app.use(express.json());
 app.use('/characters', route);
-app.use(cors());
 
 app.listen(port, () => {
   console.log(`Serve work on http://localhost:${port}`);
